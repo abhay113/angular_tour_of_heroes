460c9a977c52a42623ee56c6f5e2c20a272707a4
abhay <abhay@cateina.com>
2024-06-05 12:32:41 +0530

    in built server added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,22 +3,35 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './data/heroes';
 import { HEROES } from './data/mock-heros';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private msgService: MessageService) {}
+  private heroesUrl = 'api/heroes'; // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
+  //fetching from array
+  // getHeroes(): Observable<Hero[]> {
+  //   const heroes = of(HEROES);
+  //   this.msgService.add('HeroService : Fetched Heroes');
+  //   return heroes;
+  // }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.msgService.add('HeroService : Fetched Heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
-
   getHero(id: Number): Observable<Hero> {
     const hero = HEROES.find((h) => h.id === id)!;
-    this.msgService.add(`HeroService : fetched hero id=${id}`);
+    this.messageService.add(`HeroService : fetched hero id=${id}`);
     return of(hero);
+  }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
